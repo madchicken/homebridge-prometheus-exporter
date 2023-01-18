43d2738d8520b42cec8511764b4d553e30c718f6
Pierpaolo Follia <pfollia@gmail.com>
2023-01-18 18:55:41 +0100

    Improve build scripts (#3)
    * Create archives to update when releasing
    
    * Bump version

--- build-all.sh
+++ build-all.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
-
+rm -rf target
+rm *.tgz
 docker run --rm -v $(pwd):/app rust-compile-aarc64
 docker run --rm -v $(pwd):/app rust-compile-armv7
+cargo build --release
+
+tar cvfz homebridge-exporter-aarc64.tgz target/aarch64-unknown-linux-gnu/release/homebridge-exporter
+tar cvfz homebridge-exporter-armv7.tgz target/armv7-unknown-linux-gnueabihf/release/homebridge-exporter
+tar cvfz homebridge-exporter-darwin-arm64.tgz target/release/homebridge-exporter
